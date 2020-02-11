95060c155c7874892bf073576aa61116e5bcc92f
renanrcp <renancamerapinheiro@gmail.com>
2020-02-11 00:43:49 -0300

    some init configs

---
+++ src/main.js
@@ -0,0 +1,47 @@
+const { app,  BrowserWindow } = require('electron');
+const nativeImage = require('electron').nativeImage;
+
+const createWindow = () => {
+    const window = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: false
+        },
+        title: 'Netflix',
+        fullscreenable: true,
+        icon: nativeImage.createFromPath(__dirname + getApplicationIcon())
+    });
+    
+    window.maximize();
+
+    window.on('page-title-updated', (e) => {
+        e.preventDefault();
+    });
+
+    window.loadURL('https://www.netflix.com/');
+};
+
+const getApplicationIcon = () => {
+    if (process.platform === 'win32') {
+        return '/assets/netflix-windows-icon.ico';
+    } else if (process.platform === 'darwin') {
+        return '/assets/netflix-mac-icon.png';
+    } else {
+        return '/assets/netflix-linux-icon.png';
+    }
+};
+
+app.allowRendererProcessReuse = false;
+app.whenReady()
+    .then(() => createWindow());
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+});
